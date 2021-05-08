8e54cf4462adad99d5af7519fb66c1b8aa0fe3a2
corstianl <zhulonglingh311@163.com>
2021-05-08 13:55:12 +0800

    Initial Commit

--- 3_Implementation/src/com/LNT/COVID19/final/connection.php
+++
@@ -1,6 +0,0 @@
-<?php  
-       $servername = "localhost";  
-       $username = "root";  
-       $password = "";  
-       $conn = mysqli_connect ($servername , $username , $password,"test") or die("unable to connect to host");  
-?> 
